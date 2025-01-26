4d38353fcd5cc05aaefbe52956fa50141d207587
josuemj <98498772+josuemj@users.noreply.github.com>
2025-01-25 20:41:50 -0600

    setting user app provider with riverpod

--- favorite_places/lib/providers/user_places.dart
+++ favorite_places/lib/providers/user_places.dart
@@ -1,1 +1,15 @@
+import 'package:favorite_places/model/place.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+class UserPlacesNotifier extends StateNotifier<List<Place>> {
+  UserPlacesNotifier() : super(const []); // initial
+
+  void addPlace(String title) {
+    final newPlace = Place(title: title);
+    state = [newPlace, ...state]; // update state (list)
+  }
+}
+
+final UserPlacesProvider = StateNotifierProvider(
+  (ref) => UserPlacesNotifier(),
+);
