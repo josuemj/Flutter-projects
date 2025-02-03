d43d08d664467d8555e4c3123bb80aa7d63f7632
josuemj <98498772+josuemj@users.noreply.github.com>
2025-02-03 13:15:04 -0600

    chore -> added http

--- favorite_places/lib/widgets/location_input.dart
+++ favorite_places/lib/widgets/location_input.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
+import 'package:http/http.dart' as http;
 
 class LocationInput extends StatefulWidget {
   const LocationInput({super.key});
