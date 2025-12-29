be0b81ef99e30efc44fe991a8065445c33ea4f7b
josuemj <98498772+josuemj@users.noreply.github.com>
2025-12-28 21:06:49 -0600

    refactor 1 - feature separation

---
+++ rolling_dice_app/lib/dice_roller/dice_roller.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+
+  @override
+  _DiceRollerState createState() => _DiceRollerState();
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  int _currentDiceRoll = 1;
+
+  void _rollDice() {
+    final random = Random();
+    int number = random.nextInt(6) + 1;
+
+    setState(() {
+      _currentDiceRoll = number;
+      print('Dice rolled to $_currentDiceRoll');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset('assets/images/dice-$_currentDiceRoll.png',
+            width: 200, height: 200),
+        ElevatedButton(
+          onPressed: _rollDice,
+          child: Text('Roll Dice'),
+        ),
+      ],
+    );
+  }
+}
