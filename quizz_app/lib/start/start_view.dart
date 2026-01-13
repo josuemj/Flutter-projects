6db12f7d35397e5b6ec6a955e090ba4ce9553d55
josuemj <98498772+josuemj@users.noreply.github.com>
2026-01-13 10:48:53 -0600

    from AG implemented animation

--- quizz_app/lib/start/start_view.dart
+++ quizz_app/lib/start/start_view.dart
@@ -1,9 +1,67 @@
 import 'package:flutter/material.dart';
 
-class StartView extends StatelessWidget {
+class StartView extends StatefulWidget {
   const StartView({super.key, required this.startQuiz});
 
   final VoidCallback startQuiz;
+
+  @override
+  State<StartView> createState() => _StartViewState();
+}
+
+class _StartViewState extends State<StartView>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _logoScaleAnimation;
+  late Animation<double> _logoOpacityAnimation;
+  late Animation<Offset> _contentSlideAnimation;
+  late Animation<double> _contentOpacityAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+
+    _logoScaleAnimation = Tween<double>(begin: 0.5, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: const Interval(0.0, 0.5, curve: Curves.easeOutBack),
+      ),
+    );
+
+    _logoOpacityAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: const Interval(0.0, 0.5, curve: Curves.easeIn),
+      ),
+    );
+
+    _contentSlideAnimation =
+        Tween<Offset>(begin: const Offset(0, 0.2), end: Offset.zero).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: const Interval(0.4, 1.0, curve: Curves.easeOut),
+      ),
+    );
+
+    _contentOpacityAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: const Interval(0.4, 1.0, curve: Curves.easeIn),
+      ),
+    );
+
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,32 +70,49 @@ class StartView extends StatelessWidget {
       mainAxisSize: MainAxisSize.max,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Image.asset(
-          "assets/images/quiz-logo.png",
-          width: 300,
-          height: 300,
-          color: const Color.fromARGB(134, 255, 255, 255),
-        ),
-        Center(
-          child: Container(
-            padding: const EdgeInsets.all(20),
-            child: Text(
-              "Learn flutter the fun way",
-              style: TextStyle(
-                fontSize: 24,
-                color: Colors.white,
-              ),
+        ScaleTransition(
+          scale: _logoScaleAnimation,
+          child: FadeTransition(
+            opacity: _logoOpacityAnimation,
+            child: Image.asset(
+              "assets/images/quiz-logo.png",
+              width: 300,
+              height: 300,
+              color: const Color.fromARGB(134, 255, 255, 255),
             ),
           ),
         ),
-        TextButton.icon(
-          onPressed: () {
-            startQuiz();
-          },
-          style: TextButton.styleFrom(foregroundColor: Colors.white),
-          label: const Text("Start quiz", style: TextStyle(fontSize: 16)),
-          icon: const Icon(Icons.arrow_right_alt),
-        )
+        SlideTransition(
+          position: _contentSlideAnimation,
+          child: FadeTransition(
+            opacity: _contentOpacityAnimation,
+            child: Column(
+              children: [
+                Center(
+                  child: Container(
+                    padding: const EdgeInsets.all(20),
+                    child: const Text(
+                      "Learn flutter the fun way",
+                      style: TextStyle(
+                        fontSize: 24,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                TextButton.icon(
+                  onPressed: widget.startQuiz,
+                  style: TextButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    textStyle: const TextStyle(fontSize: 16),
+                  ),
+                  label: const Text("Start quiz"),
+                  icon: const Icon(Icons.arrow_right_alt),
+                )
+              ],
+            ),
+          ),
+        ),
       ],
     );
   }
